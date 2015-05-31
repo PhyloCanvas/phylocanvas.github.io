6d0b8a74a704d153d505f223662a027d8b692f01
Richard Goater <richard.goater27@gmail.com>
2015-05-31 01:35:05 +0100

    Experimental dev-server

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var data = require('gulp-data');
 var jade = require('gulp-jade');
 var minify = require('gulp-minify-html');
-
+var livereload = require('gulp-livereload');
 
 gulp.task('jade', function () {
   gulp.src('./templates/*.jade')
@@ -15,5 +15,20 @@ gulp.task('jade', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('templates/**/*.*', [ 'jade' ]);
+  livereload.listen();
+  gulp.watch('templates/**/*.*', [ 'jade', 'refresh' ]);
+});
+
+gulp.task('server', function () {
+  var express = require('express');
+  var app = express();
+  app.use(require('connect-livereload')());
+  app.use(express.static(__dirname, { etag: false }));
+  app.listen(4000, '0.0.0.0');
+});
+
+gulp.task('refresh', [ 'jade' ], function () {
+  setTimeout(function () {
+    livereload.reload();
+  }, 200);
 });
