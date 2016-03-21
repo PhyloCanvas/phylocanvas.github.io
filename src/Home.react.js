a32effcc1c515d773f73f33d9353671c1343ce6f
Richard Goater <richard.goater27@gmail.com>
2016-03-21 17:30:29 +0000

    More WIP for the holding page

--- src/Home.react.js
+++ src/Home.react.js
@@ -7,12 +7,13 @@ export default React.createClass({
       const Phylocanvas = require('phylocanvas-quickstart');
       const tree = Phylocanvas.createTree('phylocanvas', {
         history: false,
+        disableZoom: true,
       });
 
       tree.branchColour = '#3C7383';
       tree.highlightColour = '#3C7383';
       tree.baseNodeSize = 10;
-      tree.padding = 0;
+      tree.padding = 32;
       tree.load('/v1.x/docs/data/tree.nwk', () => {
         for (const leaf of tree.leaves) {
           leaf.setDisplay({
@@ -54,22 +55,22 @@ export default React.createClass({
             <a href="#contact">contact</a>
           </li>
         </nav>
-        <section className="flex-column">
+        <section className="home-intro">
+          <div id="phylocanvas" />
           <header>
             <h1>
               <img src="/img/Phylo.FINAL.svg" className="logo" alt="phylocanvas" />
             </h1>
             <p className="lead">Interactive tree visualisation <br />for the web.</p>
           </header>
-          <div id="phylocanvas" />
           <p className="cta">
-            v2.x docs coming soon.
+            <span>v2.x docs coming soon.</span>
           </p>
         </section>
         <footer id="contact">
           <p>Phylocanvas is maintained by<br />The Centre for Genomic Pathogen Surveillance.</p>
           <img src="/img/CGPS.white_.FINAL_.svg" />
-          <div className="acknowledgements">Github icon made by <a href="http://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
+          <div className="icon-acknowledgement">Github icon made by <a href="http://www.flaticon.com/authors/dave-gandy" title="Dave Gandy">Dave Gandy</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a>             is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0">CC BY 3.0</a></div>
         </footer>
       </div>
     );
