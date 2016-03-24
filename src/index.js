f29f45e447783a96623619191733837752d36bbd
Richard Goater <richard.goater27@gmail.com>
2016-03-24 15:55:06 +0000

    Update react router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import ReactDOMServer from 'react-dom/server';
-import { createHistory, createMemoryHistory } from 'history';
-import { Router, RoutingContext, match } from 'react-router';
+import { render } from 'react-dom';
+import { renderToString } from 'react-dom/server';
+import {
+  Router,
+  RouterContext,
+  match,
+  browserHistory,
+  createMemoryHistory,
+} from 'react-router';
 
 import routes from './routes';
 import documentTemplate from './document';
@@ -10,9 +15,7 @@ import documentTemplate from './document';
 // Client render (optional):
 if (typeof document !== 'undefined') {
   require('./css/styles.css');
-
-  const history = createHistory();
-  ReactDOM.render(<Router history={history} routes={routes} />, document.getElementById('content'));
+  render(<Router history={browserHistory} routes={routes} />, document.getElementById('content'));
 }
 
 // Exported static site renderer:
@@ -22,7 +25,7 @@ export default (locals, callback) => {
 
   match({ routes, location }, (error, redirectLocation, renderProps) => {
     callback(null, documentTemplate(
-      ReactDOMServer.renderToString(<RoutingContext {...renderProps} />)
+      renderToString(<RouterContext {...renderProps} />)
     ));
   });
 };
